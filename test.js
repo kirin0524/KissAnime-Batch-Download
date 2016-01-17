11f9143a7df2a3264928de71f2dfb7fe725cfc8a
Chester Ayala <flippyveteranstorm@icloud.com>
2016-01-17 22:01:59 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var redirect_url = 'http://2.bp.blogspot.com/mTGn0Ri2HHrMQbEzkFKI0V_b2SYA1tSpReV1vYj21Q=m36';
 var response_url;
-$.ajax({
+jQuery.ajax({
         type: "GET",
         url: redirect_url,
         success : function( data, textStatus, jqXHR){
